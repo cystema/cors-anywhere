9dbce1fcfafb940fd928578e5d936da3007ce3e2
Shubham Mazumder <shubham.mazumder@gmail.com>
2024-09-22 04:55:12 -0600

    add url

--- server.js
+++ server.js
@@ -21,8 +21,7 @@ var checkRateLimit = require('./lib/rate-limit')(process.env.CORSANYWHERE_RATELI
 
 var cors_proxy = require('./lib/cors-anywhere');
 cors_proxy.createServer({
-  originBlacklist: originBlacklist,
-  originWhitelist: originWhitelist,
+  originWhitelist: ['https://www.shubh.ink', 'localhost:3000'],
   requireHeader: ['origin', 'x-requested-with'],
   checkRateLimit: checkRateLimit,
   removeHeaders: [
